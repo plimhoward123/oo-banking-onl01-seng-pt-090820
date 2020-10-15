1249f6ad008014529be7b4132e12aac78df8cc93
Plimon Howard <plimon.howard@gmail.com>
2020-10-15 04:11:13 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -16,7 +16,7 @@ class BankAccount
     "Your balance is $#{@balance}."
   end
   def valid?
-    if (@status == "open")
+    if (@status == "open" && @balance > 0)
       true
     else
       false
