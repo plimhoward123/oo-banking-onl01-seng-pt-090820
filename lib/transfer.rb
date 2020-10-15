25a74e4be689f7867cdabcbb9c2e7e794414475d
Plimon Howard <plimon.howard@gmail.com>
2020-10-15 04:28:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
 
   def execute_transaction
     if (@sender.balance > @amount)
-      if (self.valid?)
+      if (valid?)
         @receiver.balance += @amount
         @sender.balance -= @amount
         @status = "complete"
@@ -23,6 +23,10 @@ class Transfer
         @status = "rejected"
         "Transaction rejected. Please check your account balance."
       end
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
     end
   end
 end
