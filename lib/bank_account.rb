2698806f6ec63a8bb279383ce0967fdac23546d6
Plimon Howard <plimon.howard@gmail.com>
2020-10-15 04:11:53 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,18 +8,23 @@ class BankAccount
     @status = "open"
   end
 
-
   def deposit(amnt)
     self.balance += amnt
   end
+  
   def display_balance
     "Your balance is $#{@balance}."
   end
+  
   def valid?
     if (@status == "open" && @balance > 0)
       true
     else
       false
     end
+  end
+
+  def close_account
+    @status = "closed"
   end
 end
