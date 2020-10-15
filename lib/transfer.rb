ee02682e47643ea0aeab3e7d44f0ee9d9d0a5b3a
Plimon Howard <plimon.howard@gmail.com>
2020-10-15 04:22:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,8 @@ class Transfer
       @sender.balance -= @amount
       @status = "complete"
    else
-     @stats = "Transaction rejected. Please check your account balance."
+     @stats = "rejected"
+     puts "Transaction rejected. Please check your account balance."
    end
   end
 end
